d88a18b28236c09e52a175f5d18523d6969d84ce
Sarah Samora <sarahsamora@mail.com>
2020-05-22 20:13:20 -0700

    ocp code

---
+++ src/lab/assignment/afterrefactoring/MochaCoffee.java
@@ -0,0 +1,13 @@
+package lab.assignment.afterrefactoring;
+
+public class MochaCoffee {
+
+    public void addIngredients() {
+        System.out.println("Adding cocoa powder");
+        System.out.println("Adding hot milk");
+    }
+
+    public void finalTouch(){
+        System.out.println("Adding sugar");
+    }
+}
---
+++ src/lab/assignment/afterrefactoring/clientcode/CoffeeClient.java
@@ -0,0 +1,19 @@
+package lab.assignment.beforerefactoring.clientcode;
+
+import lab.assignment.beforerefactoring.CoffeeMaker;
+
+public class CoffeeClient {
+
+
+    public static void main(String[] args) {
+        CoffeeMaker starbuzz = new CoffeeMaker();
+        starbuzz.prepareMochaCoffee();
+
+        System.out.println("********************");
+
+        starbuzz = new CoffeeMaker();
+        starbuzz.prepareCapuccinoCoffee();
+
+    }
+
+}
